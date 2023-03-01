9d06000cbc150ea4b7bbb5c84e123f8df8365f91
Vitor 0-0 <75373270+VitorBuenoOliveira@users.noreply.github.com>
2023-02-28 22:58:34 -0300

    Up1Codicioanais

--- Condicioanais.py
+++ Condicioanais.py
@@ -4,3 +4,28 @@ Cal = massa / (h**2)
 
 if 18.5 < Cal > 24.9:
   print('Seu Peso é normal')
+
+
+
+  ###
+massa = float(input("Digite Sua Massa: "))
+Altura = float(input("Digite Sua Altura: "))
+
+Cal =  massa / (Altura**2)
+
+if Cal < 17:
+  print("Você Está Abaixo do Peso ;-;")
+if 17.5 < Cal < 18.49:
+  print("Você Está no Peso normal")
+if 18.50 < Cal < 25:
+  print("Você Está Acima do Peso")
+if 25 < Cal < 30:
+  print("Você Está Com Obesidade I")
+if 31 < Cal < 35:
+  print("Você Está Com Obesidade II (!!!Severa)")
+if 39.99 < Cal < 40:
+  print("Você Está Com Obesidade III (!!!Mórbida!!!)")
+
+
+
+  ###
