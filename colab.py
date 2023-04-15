4fba7d6bc8b084d122efd58cf2063d802c4053d5
Vitor 0-0 <75373270+VitorBuenoOliveira@users.noreply.github.com>
2023-04-14 21:05:18 -0300

    Lista4

--- colab.py
+++ colab.py
@@ -52,5 +52,42 @@
 # else:
 #     print('A frase digitada não é um palíndromo')
 
+# Escreva um programa que receba uma string como entrada e retorne True se a string contiver apenas caracteres únicos, ou False caso contrário.
+
+# def verifica_caracteres_unicos(): # Só Funciona no Notebook
+#     """
+#     Verifica se uma string contém apenas caracteres únicos.
+
+#     Retorno:
+#     True se a string contém apenas caracteres únicos, False caso contrário.
+#     """
+#     string = input("Digite a string a ser verificada: ")
+#     if len(set(string)) == len(string):
+#         return True
+#     else:
+#         return False
+
+# verifica_caracteres_unicos()
+
+# Escreva um programa que receba uma string como entrada e retorne a mesma string com todas as letras consecutivas duplicadas reduzidas a uma única letra.
 
 
+def reduzir_consecutivas(string):
+    """
+    Reduz todas as letras consecutivas duplicadas de uma string para uma única letra.
+
+    Argumentos:
+    string -- a string a ser reduzida.
+
+    Retorno:
+    A string com todas as letras consecutivas duplicadas reduzidas a uma única letra.
+    """
+    nova_string = ''
+    for i in range(len(string)):
+        if i == 0 or string[i] != string[i-1]:
+            nova_string += string[i]
+    return nova_string
+
+string = input("Digite a string a ser reduzida: ")
+nova_string = reduzir_consecutivas(string)
+print(nova_string)
