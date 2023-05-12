c84ecf743faee2d5fb179d81550398d7807339c1
Vitor 0-0 <75373270+VitorBuenoOliveira@users.noreply.github.com>
2023-05-11 21:34:12 -0300

    save10

---
+++ Introduções/Dicionário.py
@@ -0,0 +1,1 @@
+# https://colab.research.google.com/github/caio-c-silva/introducao_programacao/blob/main/notebooks_aulas/Dicion%C3%A1rios.ipynb#scrollTo=33a1a7f9-d633-4aca-82ee-c333d21bd1cd
---
+++ Introduções/exercfix.py
@@ -0,0 +1,31 @@
+artistas = {'The Weeknd' : 106.96,'Taylor Swift': 85.36 , 'Ed Sheeran': 84.09, 'Miley Cyrus': 80.77, 'Shakira': 76.90, 'Rihanna': 75.91, 'Ariana Grande': 74.82,'Justin Biber': 74.46, 'Drake': 70.54 , 'David Guerra': 70.50 }
+
+# artistas
+
+# soma = 0 
+# for artista in artistas:
+#     soma += artistas[artista]
+# somada
+
+# mais_ouvidos = []
+# for artista in artistas:
+#     if artistas[artista] > 75:
+#         mais_ouvidos.append(artista)
+# mais_ouvidos
+
+# for artista in artistas:
+#     artista[artista] += 5
+# artistas
+
+###BETA####
+# h2o = 6
+# ar = 0
+# lista = {'coluna': 0,'coluna2': 8,'coluna3':  2 ,'coluna4' : 0, 'coluna5': 0 ,'coluna6': 10, 'coluna7': 0,'coluna8': 0,'coluna9': 5, 'coluna10':1,'coluna11':  6}
+
+
+# for valor in lista :
+#     if lista[valor] >= h2o:
+#         lista[valor] == 0
+#     else:
+#         c = h2o - lista[valor]
+#         print(c)
