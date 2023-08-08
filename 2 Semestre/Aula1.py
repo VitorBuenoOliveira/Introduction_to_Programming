b5a82f70eabc842ba637b70a0396cabb78733fd3
Vitor 0-0 <75373270+VitorBuenoOliveira@users.noreply.github.com>
2023-08-08 20:27:20 -0300

    Update Aula1.py

---
+++ 2 Semestre/Aula1.py
@@ -0,0 +1,20 @@
+ano  = int(input('Digite o ano que vc nasceu: '))
+ano_atual = int(input('Digite o ano atual que vc está: '))
+
+idade =  ano_atual - ano 
+
+
+if idade >= 18 :
+    print(f'Maior de idade :{idade}')
+elif idade <= 2:
+    print('Bebe')
+else:
+    print(f'Menor de idade :{idade}')
+
+
+
+# Aprenda:
+# -Ler/Ver 
+# -Enteder 
+# -Contemplar 
+# -Explicar
