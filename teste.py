bc4ee975b466f267a0b1ceca03a2b7ada0100fc9
Vitor 0-0 <75373270+VitorBuenoOliveira@users.noreply.github.com>
2023-08-15 17:19:41 -0300

    transforma em binario

--- teste.py
+++ teste.py
@@ -1,10 +1,5 @@
-def disemvowel(string_):
-    string_ = string_.replace('a','')
-    string_ = string_.replace('e','')
-    string_ = string_.replace('i','')
-    string_ = string_.replace('O','')
-    string_ = string_.replace('u','')
-    a = print(string_)
-    return a 
-
-disemvowel('This website is for losers LOL!')
+# def add_binary(a,b):
+#     result = a + b 
+    
+#     binary_result = bin(result)[2:]
+#     return binary_result
