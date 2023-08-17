6dc6af9453dfec7a235e17ab817668c230cd3264
Vitor 0-0 <75373270+VitorBuenoOliveira@users.noreply.github.com>
2023-08-17 20:33:42 -0300

    aula3

---
+++ 2 Semestre/Aula3.py
@@ -0,0 +1,37 @@
+# print([i if i % 2==0 else'vixe' for i in range(10)])
+
+# a = ['D' , 'u' , 'd' , 'a']
+
+# b = ['arroz','feijão','farinha']
+
+# c = [2,4,6,8]
+# print(a[1])
+# print(b[1])
+# print(c[1])
+# print(c[2:])
+# print(len(a))
+
+#Dicionario
+
+# d = {'nome':'Eduardo','idade':29}
+
+# print(d['nome'])
+
+# print(len(d))
+
+
+
+# a= ['d','u','d','a'],
+ 
+# b = ['a','fe','fai']
+
+# c = [2,4,6,'a']
+
+# e = [a , b , c]
+# x = e[2][1]
+# print(x)
+# y = e[2][3][3]
+# print(y)
+
+def imprime_linha(n1,carac1,n2,carac2):
+    print(carac1 * n1 + carac2 * n2)
--- teste.py
+++ teste.py
@@ -3,3 +3,18 @@
     
 #     binary_result = bin(result)[2:]
 #     return binary_result
+
+
+# Tira todos os caracteres especificos
+
+# def filter_list(list):
+#     new_list = []
+#     for item in list:
+#         if type(item) != str:
+#             new_list.append(item)
+#     return new_list
+# lista=['a','abc',1,2,3,'cde',5.6]
+
+# filter_list(lista)
+
+
