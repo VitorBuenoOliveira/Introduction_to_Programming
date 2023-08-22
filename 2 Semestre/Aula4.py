9f8258a96f1cd8ed5f307b17808de42a5ac180ed
Vitor 0-0 <75373270+VitorBuenoOliveira@users.noreply.github.com>
2023-08-22 20:13:19 -0300

    Aula 4 | Livro capitulo 4

---
+++ 2 Semestre/Aula4.py
@@ -0,0 +1,63 @@
+# def num_sei():
+#     a = 10 * 0.2
+
+#     return a
+
+
+
+
+
+
+# def numeric(valor,perct):
+#     conta = (valor*perct)/100
+#     p = print(conta)
+#     return p 
+
+# valor = float(input('Digite o valor: '))
+# perct = float(input('Digite a %: '))
+
+
+###################
+# Exercicio do livro cap 04
+# import math
+# import math
+
+# def imprimir_tabela_raizes_quadradas(inicio, fim, incremento):
+#     print("Número\tRaiz Quadrada")
+#     print("=" * 20)
+    
+#     numero = inicio
+#     while numero <= fim:
+#         raiz_quadrada = math.sqrt(numero)
+#         print(f"{numero}\t{raiz_quadrada:.4f}")
+#         numero += incremento
+
+# inicio = 1
+# fim = 100
+# incremento = 10
+
+# imprimir_tabela_raizes_quadradas(inicio, fim, incremento)
+
+# from math import sqrt
+
+
+
+
+def exercicio_4_3(numero):
+    print(f'numero = {numero}')
+    print(f'{numero}² = {numero ** 2 } ')
+    print(f'{numero}³ = {numero ** 3 }')
+
+valor = int(input('Digite um numero: '))
+
+exercicio_4_3(valor)
+
+
+
+
+
+
+
+
+
+
