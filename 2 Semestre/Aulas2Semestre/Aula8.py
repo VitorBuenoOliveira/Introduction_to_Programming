16064e47d1e16db3c82f2f4f10b21bdfcf48d4e9
Vitor 0-0 <75373270+VitorBuenoOliveira@users.noreply.github.com>
2023-09-14 20:30:28 -0300

    Aula8

---
+++ 2 Semestre/Aulas2Semestre/Aula8.py
@@ -0,0 +1,52 @@
+# lista = [0,1,2,3,4,5,6,7,8,9]
+# lista = [5,6,7,8,9,0,1,2,3,4]
+
+# ultimo = lista.pop()
+# lista.insert(0,ultimo)
+
+# def girar_lista(ata:int, lista:list) -> list :
+#     for _ in range(ata):
+#         ultimo = lista.pop()
+#         lista.insert(0,ultimo)
+#     return lista
+# lista = [1,2,3,4,5,6,7,8,9]
+# print(girar_lista(5,lista))
+
+# lista = []
+# for i in range(10):
+#     lista.append(i*2)
+#     print(lista)
+
+# lista = [i*2 for i in range(10)]
+# print(lista)
+
+# lista=[i if i%2==0 else 'vixe' for i in range(20)]
+# print(lista)
+
+# trid = [[[j for j in range(i)]for i in range(3)]]
+# print(trid)
+
+
+# lista_trid = [
+#     {
+#         [1,2,3]
+#     }
+# ]
+
+
+
+# l1 = ['d','u','b']
+# l2 = ['a','r','t']
+# l3 = ['s','x','w']
+
+# lm1 = ['m','n','o']
+# lm2 = ['','','g']
+# lm3 = ['','','k']
+
+# tabela = [l1,l2,l3]
+# tm = [lm1,lm2,lm3]
+# print(tabela)
+
+# cubo = [tabela,tm]
+
+
